ceca5deaf12304835d08e7aec9ebac18e7ad4bfb
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-12-01 00:11:36 +0100

    Create models and basic stuff of MainWindow

---
+++ Models/Campaign.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FuturumCampaign.Models
+{
+    public class Campaign
+    {
+        public string Name { get; set; } = default!;
+
+        public List<Keyword> Keywords { get; set; } = default!;
+
+        public List<Town> Towns { get; set; } = default!;
+    }
+}
---
+++ Models/Town.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuturumCampaign.Models
+{
+    public class Town
+    {
+        public string Name { get; set; } = default!;
+
+        public double Population { get; set; }
+
+        public string? Description { get; set; }
+
+        public double Radius { get; set; }
+    }
+}
