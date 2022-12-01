d65ee5543147b4e06b7980f215d3367f37769f77
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-12-01 01:07:23 +0100

    Correct styles in MainWindow

--- ViewModels/MainWindowVM.cs
+++ ViewModels/MainWindowVM.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FuturumCampaign.ViewModels
+﻿namespace FuturumCampaign.ViewModels
 {
+    /// <summary>
+    /// View model for MainWindow.xaml
+    /// </summary>
     public class MainWindowVM
     {
-
     }
 }
