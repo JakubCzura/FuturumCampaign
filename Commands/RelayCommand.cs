3890968108ff6a12c305e10e943d0067472d254a
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-12-01 01:34:34 +0100

    Correct styles in MainMenu

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace FuturumCampaign.Commands
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> canExecute;
+        private readonly Action<T> execute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<T> action) : this(action, canExecute => true)
+        {
+            execute = action;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                //return true;
+                return canExecute((T)parameter);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Method error");
+                return false;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            try
+            {
+                execute?.Invoke((T)parameter);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Method error");
+            }
+        }
+    }
+}
