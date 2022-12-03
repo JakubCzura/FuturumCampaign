c9833bc3b1e2f1cc7490adb50b896a1d7ba248b1
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-12-03 23:30:20 +0100

    Change font size for campaigns

--- Models/Campaign.cs
+++ Models/Campaign.cs
@@ -4,10 +4,7 @@ namespace FuturumCampaign.Models
 {
     public class Campaign
     {
-        public Campaign()
-        {
-            Keywords = new();
-        }
+        public Campaign() { }
 
         public Campaign(decimal bidAmount, decimal minAmount, decimal fund, List<string> keywords, string name, double radius, string status, string town, string productName, string description)
         {
@@ -23,18 +20,13 @@ namespace FuturumCampaign.Models
             Product.Description = description;
         }
 
-        public Product Product { get; set; } = new();
         public decimal BidAmount { get; set; } = default!;
         public decimal Fund { get; set; } = default!;
-        public List<string> Keywords { get; set; } = default!;
+        public List<string> Keywords { get; set; } = new();
         public decimal MinAmount { get; set; } = default!;
         public string Name { get; set; } = default!;
-        public Town Town { get; set; } = new();
-
+        public Product Product { get; set; } = new();
         public Status Status { get; set; } = new();
-        //public double Radius { get; set; } = default!;
-        
-        //public string Status { get; set; } = default!;
-        //public string Town { get; set; } = default!;
+        public Town Town { get; set; } = new();
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -7,14 +7,7 @@ using System.Threading.Tasks;
 namespace FuturumCampaign.Models
 {
     public class Product
-    {
-        public Product() { }
-        public Product(string productName, string description)
-        {
-            ProductName = productName;
-            Description = description;
-        }
-
+    {      
         public string ProductName { get; set; } = default!;
 
         public string Description { get; set; } = default!;
